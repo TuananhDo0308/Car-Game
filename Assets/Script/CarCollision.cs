52808b611b0da4f4d9d8d4309a3cf7dfb6603b51
TuananhDo0308 <TuananhDo0308@gmail.com>
2023-10-09 12:54:46 +0700

    hello

--- Assets/Script/CarCollision.cs
+++ Assets/Script/CarCollision.cs
@@ -6,7 +6,7 @@ public class CarCollision : MonoBehaviour
 {
     // Start is called before the first frame update
     private void OnCollisionEnter2D(Collision2D other) {
-        if (other.transform.name == "Track"){
+        if (other.transform.tag == "Track"){
             CarManager.isGameOver = true;
         }
     }
